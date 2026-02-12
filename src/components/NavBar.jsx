2b185a5a7c7d174fb8ef759b1559db1cb86a6f08
Ashwin-Wilson <ashwinwilson24@gmail.com>
2026-02-13 00:39:48 +0530

    fix: nav bar logo not displayed fix 1

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
         {/* Logo Section */}
         <a href="/">
           <img
-            src="public/images/cea-logo-black.png"
+            src="/public/images/cea-logo-black.png"
             className='w-45 md:w-50'
             alt="cea-logo" />
         </a>
