7cd26be27ed2de59ec4175979be02bf3a6dcfd73
Ashwin-Wilson <ashwinwilson24@gmail.com>
2026-02-12 21:48:38 +0530

    fix: master sync issue from previous commit fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import { useState } from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from './components/NavBar'
-import Footer from './components/Footer';
-import LandingPage from './pages/LandingPage'
-import BoardOfGovernors from './pages/Administration/BoardOfGovernors';
 import Navbar from "./components/NavBar";
 import Footer from "./components/Footer";
 import LandingPage from "./pages/LandingPage";
@@ -21,16 +17,6 @@ function App() {
 
   return (
     <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/board-of-governors" element={<BoardOfGovernors />} />
-          {/* <Route path="*" element={<NotFound />} /> */}
-        </Routes>
-        <Footer />
-
-      </BrowserRouter>
-  )
       <Navbar />
       <Routes>
         <Route path="/" element={<LandingPage />} />
